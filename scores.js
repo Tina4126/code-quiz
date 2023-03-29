c0397c30d43957dfa36bb009a17dc5ae69cb2646
Sumayyah Mosafeer <119902274+Tina4126@users.noreply.github.com>
2023-03-29 22:47:53 +0100

    scores

---
+++ scores.js
@@ -0,0 +1,14 @@
+const scores = [
+    {
+      name: "Barbra",
+      score: 7
+    },
+    {
+      name: "Jenine",
+      score: 18
+    },
+    {
+      name: "Yvonne",
+      score: 19
+    }
+  ];
