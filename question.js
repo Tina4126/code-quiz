fbaf49644c47dc3c5a5ecd2deea16bd7055cf8c2
Sumayyah Mosafeer <119902274+Tina4126@users.noreply.github.com>
2023-03-29 22:47:03 +0100

    added questions

--- question.js
+++ question.js
@@ -8,4 +8,19 @@ const questions = [{
     choices: ["number", "boolean", "string", "array"],
     answer: "array"
 },
-]
+{
+    question: "What was the most popular programming language in 2021?    ",
+    choices: ["Python", "JavaScript", "Python", "C#"],
+    answer: " JavaScript "
+},
+  {
+    question: " Which of the following is a popular text editor used for coding? ",
+    choices: ["Photoshop ", " Illustrator ", " Adobe Premiere Pro ", " Visual Studio Code "],
+    answer: " Visual Studio Code "
+},
+  {
+    question: " Which of the following is not a programming language?",
+    choices: ["JavaScript", "HTML", "CSS", "MySQL"],
+   answer: " MySQL "
+}
+];
