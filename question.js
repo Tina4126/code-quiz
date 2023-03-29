f5475830128e0e651ad92111721070fc96c63a21
Sumayyah Mosafeer <119902274+Tina4126@users.noreply.github.com>
2023-03-29 22:46:17 +0100

    questions added

---
+++ question.js
@@ -0,0 +1,11 @@
+const questions = [{
+    question: "What does the acronym API stand for?", 
+    choices: ["Application Programming Interface", "Automated Programming Interface", "Advanced Programming Interface", "All-Purpose Interface"],
+    answer: "Application Programming Interface"
+},
+{
+    question: "Which of the following is NOT a data type in JavaScript",
+    choices: ["number", "boolean", "string", "array"],
+    answer: "array"
+},
+]
